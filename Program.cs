4b436c2053e8aac8fd9bd6fb42d3f0c28f092384
mosmee21 <119822573+mosmee21@users.noreply.github.com>
2023-01-27 12:44:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            Console.WriteLine(person2.DisplayPersonInfo());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person1.ChangeFavoriteColour());
+            Console.WriteLine(person4.GetAgeInTenYears());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+            Console.WriteLine(person3.ToString());
+
+
+
+            Console.ReadLine();
+        }
+    }
+    }
+
+    
---
+++ person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Lab1
+{
+    internal class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName;set => firstName = value; }
+        public string LastName { get => lastName;set => lastName = value; }
+        public int Age { get => age; set => age = value;}
+        public string FavoriteColor { get => favoriteColor; set => favoriteColor = value;}
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+        public Person(int id, string firstName, string lastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            Age = age;
+            FavoriteColor = favoriteColor;
+            IsWorking = isWorking;
+            
+        }
+        public  override string ToString()
+        {
+            string formatted = "";
+
+            formatted += " PersonID:" + id + "\n" ;
+            formatted += " First name:" + firstName + "\n";
+            formatted += " Last name:" + lastName + "\n";
+            formatted += " favorite color :" + favoriteColor + "\n";
+            formatted += " Age :" + age + "\n";
+            formatted += " IsWorking :" + isWorking;
+
+            return formatted;
+        }
+        public string DisplayPersonInfo()
+        {
+            string formatted = "";
+
+            formatted += id + ":" + firstName + " "+  lastName + "'s" + "favorite color is" + " " + favoriteColor;
+           
+
+            return formatted;
+        }
+        public string ChangeFavoriteColour()
+        {
+            string formatted = "";
+
+            favoriteColor = "White";
+
+            formatted += DisplayPersonInfo();
+
+
+            return formatted;
+
+        }
+        public string GetAgeInTenYears()
+        {
+            string formatted = "";
+
+            age = age + 10;
+
+            formatted += firstName + lastName + "'s" + "Age in 10 years is:" + age;
+
+            age = age - 10;
+            return formatted;
+        }
+
+    }
+}
